4f0788a427261de4ea81aef57f964ded2d377343
Eduardo Pinheiro <38962010+eduardo-pinheiro@users.noreply.github.com>
2023-06-13 08:41:56 -0300

    update compiler

--- compilador.sh
+++ compilador.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-clear
-pdflatex $1.tex
-#biber $1.bcf
-bibtex $1.aux
-makeindex $1.nlo -s nomencl.ist -o $1.nls
-pdflatex $1.tex
-pdflatex $1.tex
-atril $1.pdf &
-rm -f *.aux *.dvi *.bbl *.blg *.nlo *.nls *.lot *.lof *.toc *.out *.bak *.xml *.ilg *.log
-
-
+latexmk -quiet -bibtex -pvc -f -pdf -pdflatex="pdflatex -synctex=1 -interaction=nonstopmode" principal.tex
